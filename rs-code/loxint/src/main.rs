2268e26c0864b11e2cf23870842e9bd8b45050c3
Matti Astrand <mattiastr@gmail.com>
2024-08-26 19:57:34 -0500

    Dump code into one main.rs

--- rs-code/loxint/src/main.rs
+++ rs-code/loxint/src/main.rs
@@ -1,7 +1,8 @@
-use std::io::Write;
-
 use loxlang::parser;
+use loxlang::parser::Parser;
 use loxlang::scanner::parse_tokens;
+use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
     print!("> ");
@@ -19,8 +20,26 @@ fn main() {
         eprintln!("Unparsed tokens");
     }
     println!("{:?}", e);
-    let env = rs_code::eval_expr::EvalEnv {
+    let env = loxlang::eval_expr::EvalEnv {
         values: std::collections::HashMap::new(),
     };
     println!("{:?}", loxlang::eval_expr::eval(&e, &env));
+
+    if false {
+        let args = Opts::parse();
+        // read a file into a string and parse a program
+        let source = std::fs::read_to_string(&args.file).unwrap();
+        // parse a program from source
+        let (rest, tokens) = parse_tokens(&source).unwrap();
+        assert_eq!(rest, "");
+        let mut parser = Parser::new(&tokens);
+        let program = parser.parse_program().unwrap();
+    }
+}
+
+// Use clap to parse command line arguments
+use clap::Parser as ClapParser;
+#[derive(ClapParser)]
+struct Opts {
+    file: PathBuf,
 }
