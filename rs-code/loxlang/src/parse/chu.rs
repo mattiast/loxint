60a23e35b0786b55514e112a97405891c45c898c
Matti Astrand <mattiastr@gmail.com>
2025-04-20 16:36:34 -0400

    Parse number

--- rs-code/loxlang/src/parse/chu.rs
+++ rs-code/loxlang/src/parse/chu.rs
@@ -1,9 +1,16 @@
-use crate::syntax::BOperator;
+use crate::syntax::{Annotated, BOperator, Expression};
 use chumsky::prelude::*;
 
-fn parse_bop<'src>() -> impl Parser<'src, &'src str, BOperator> {
+// Precedence levels for operators
+const PRECEDENCE: [(BOperator, i32); 4] = [
+    (BOperator::PLUS, 1),
+    (BOperator::MINUS, 1),
+    (BOperator::STAR, 2),
+    (BOperator::SLASH, 2),
+];
+
+fn parse_bop<'src>() -> impl Parser<'src, &'src str, BOperator> + Clone {
     choice((
-        // All of the basic instructions are just single characters
         just('+').to(BOperator::PLUS),
         just('-').to(BOperator::MINUS),
         just('*').to(BOperator::STAR),
@@ -11,12 +18,27 @@ fn parse_bop<'src>() -> impl Parser<'src, &'src str, BOperator> {
     ))
 }
 
+fn parse_number<'src>() -> impl Parser<'src, &'src str, Expression<'src, &'src str, ()>> + Clone {
+    text::digits(10)
+        .to_slice()
+        .map(|whole: &'src str| Expression::NumberLiteral(whole.parse::<f64>().unwrap()))
+}
+
 #[test]
 fn test_parse_bop() {
     let parser = parse_bop();
-    let r = parser.parse("hello");
     assert_eq!(parser.parse("+").unwrap(), BOperator::PLUS);
     assert_eq!(parser.parse("-").unwrap(), BOperator::MINUS);
     assert_eq!(parser.parse("*").unwrap(), BOperator::STAR);
     assert_eq!(parser.parse("/").unwrap(), BOperator::SLASH);
+}
+
+#[test]
+fn test_parse_number() {
+    let parser = parse_number();
+    let result = parser.parse("123").unwrap();
+    match result {
+        Expression::NumberLiteral(n) => assert_eq!(n, 123.0),
+        _ => panic!("Expected NumberLiteral"),
+    }
 }
